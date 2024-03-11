5bc10380798e8f237c5bdb83e0794600198f8970
Abubakar1010 <abubakar850772@gmail.com>
2024-03-11 21:03:01 +0600

    finished project with added counter limitation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [counter, setCounter] = useState(0)
+
+  function handleDecrease(){
+    if (counter <= 0) {
+      return
+    }
+    let change = counter - 1
+    setCounter(change)
+  }
+  function handleIncrease(){
+    if (counter >= 20) {
+      return
+    }
+    let change = counter + 1
+    setCounter(change)
+  }
+
   return (
     <>
-      <h1>Project Setup Done!</h1>
+      <h1>Making Counter With React Js</h1>
+
+      <h3>Count:{counter}</h3>
+      <button onClick={handleIncrease}>Increase {counter}</button>
+      <button onClick={handleDecrease}>Decrease {counter}</button>
     </>
   )
 }
